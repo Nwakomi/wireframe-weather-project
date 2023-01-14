2c33b17ebd563e822ef79c62d15a198da8809b96
Nwakomi <121190047+Nwakomi@users.noreply.github.com>
2023-01-14 10:29:59 +0300

    removed all Fahrenheit elements

--- src/app.js
+++ src/app.js
@@ -113,34 +113,14 @@ function handleSubmit(event) {
   search(cityInput.value);
 }
 
-function displayFahrenheitTemperature(event) {
-  event.preventDefault();
-  let temperatureElement = document.querySelector("#temperature");
-  celciusLink.classList.remove("active");
-  fahrenheitLink.classList.add("active");
-  let fahrenheitTemperature = (celciusTemperature * 9) / 5 + 32;
-
-  temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
-}
-
 function displayCelciusTemperature(event) {
   event.preventDefault();
 
-  celciusLink.classList.add("active");
-  fahrenheitLink.classList.remove("active");
   let temperatureElement = document.querySelector("#temperature");
   temperatureElement.innerHTML = Math.round(celciusTemperature);
 }
 
-let celciusTemperature = null;
-
 let form = document.querySelector("#search-form");
 form.addEventListener("submit", handleSubmit);
-
-let fahrenheitLink = document.querySelector("#fahrenheit");
-fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
-
-let celciusLink = document.querySelector("#celcius");
-celciusLink.addEventListener("click", displayCelciusTemperature);
 
 search("Kuwait City");
